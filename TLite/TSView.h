0fcf43f147b4d4aba8e6b304f993e7e7a008bdda
xzhou99 <xzhou99@68d7c30b-f4ff-f40d-5895-593175036387>
2011-06-02 04:22:55 +0000

    display both simulation and observation results for NGSIM data

--- TLite/TSView.h
+++ TLite/TSView.h
@@ -9,6 +9,7 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+#define _MAX_LANE_SIZE 7
 
 class CTimeSpaceView : public CView
 {
@@ -19,8 +20,13 @@ protected: // create from serialization only
 // Attributes
 public:
 
+	int m_NumberOfVehicles_lane;
+
+	int m_lane_size;
 	// processing NGSim data
 	bool m_bShowSimulationDataFlag;
+
+	bool m_bShowBothResultsFlag;
 
 	bool m_bColorBySpeedFlag; 
 	VehicleSnapshotData ** m_VehicleSnapshotAry;
@@ -32,13 +38,16 @@ public:
 
 	float *** m_VehicleSnapshotAry_Lane; // simulation
 
-	int StartTimeLane[7];
-	int EndTimeLane[7];
+	int ** m_VehicleStartTimeAry_Lane; // simulation
+	int ** m_VehicleEndTimeAry_Lane; // simulation
 
-	float StartLocalYLane[7];
-	float EndLocalYLane[7];
+	int StartTimeLane[_MAX_LANE_SIZE];  // for the first vehicle
+	int EndTimeLane[_MAX_LANE_SIZE];
 
-	int VehicleSizeLane[7];
+	float StartLocalYLane[_MAX_LANE_SIZE];
+	float EndLocalYLane[_MAX_LANE_SIZE];
+
+	int VehicleSizeLane[_MAX_LANE_SIZE];
 
 	int m_SelectLaneID;
 
@@ -157,6 +166,10 @@ public:
 	afx_msg void OnNgsimdataColorspeed();
 	afx_msg void OnUpdateNgsimdataColorspeed(CCmdUI *pCmdUI);
 	afx_msg void OnNgsimdataCarfollowingsimulation();
+	afx_msg void OnNgsimdataShowsimulatedresults();
+	afx_msg void OnUpdateNgsimdataShowsimulatedresults(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateNgsimdataShowbothobservedandsimulationresults(CCmdUI *pCmdUI);
+	afx_msg void OnNgsimdataShowbothobservedandsimulationresults();
 };
 
 
